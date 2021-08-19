447f49802c93d76a6d682b9dd5bcb7ac083a5e62
shiv_nbt <shivnbt82@gmail.com>
2021-08-19 12:04:43 +0530

    schedule_start

--- cronp/cronapp/apps.py
+++ cronp/cronapp/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class CronappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'cronapp'
+
+    def ready(self):
+        from .views import start
+        start()
--- cronp/cronapp/views.py
+++ cronp/cronapp/views.py
@@ -2,18 +2,21 @@ from django.http.response import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponse
 from apscheduler.schedulers.background import BackgroundScheduler
-from datetime import datetime
+from datetime import date, datetime
+import requests
 # Create your views here.
 
 
 def index(request):
+    return HttpResponse(datetime.now())
 
-    return HttpResponse(datetime.now)
-
-
-
+def task_add():
+    print('Executing', datetime.now())
 
 def start():
+    print('asdd')
     scheduler = BackgroundScheduler()
-    scheduler.add_job(index, 'interval', seconds=10)
+    scheduler.add_job(task_add, 'interval', seconds=10)
     scheduler.start()
+
+# start()
