7d9fca98d97150889f301f99c7be3996ddbcdb94
shiv_nbt <shivnbt82@gmail.com>
2021-08-28 12:20:32 +0530

    'view'

--- cronp/cronapp/views.py
+++ cronp/cronapp/views.py
@@ -4,14 +4,18 @@ from django.http import HttpResponse
 from apscheduler.schedulers.background import BackgroundScheduler
 from datetime import date, datetime
 import requests
+from django.shortcuts import redirect
 # Create your views here.
 
 
 def index(request):
+    print("view")
     return HttpResponse(datetime.now())
 
 def task_add():
+    f'curl -X GET "http://127.0.0.1:8000/"'
     print('Executing', datetime.now())
+  
 
 def start():
     print('asdd')
@@ -19,4 +23,4 @@ def start():
     scheduler.add_job(task_add, 'interval', seconds=10)
     scheduler.start()
 
-# start()
+    
